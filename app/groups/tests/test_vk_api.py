721aac6ead603dc5e8bdf9314d74782f20d5dc4a
Aleksandr Chilikin <majestic905@gmail.com>
2022-03-21 13:31:51 +0300

    Rename fetch_members_count_info -> fetch_groups_info; update group title as well in celery task

--- app/groups/tests/test_vk_api.py
+++ app/groups/tests/test_vk_api.py
@@ -1,5 +1,5 @@
 import pytest
-from groups.vk_api import fetch_members_count_info, fetch_group_info
+from groups.vk_api import fetch_groups_info, fetch_group_info
 
 
 @pytest.mark.asyncio
@@ -18,23 +18,22 @@ async def test_fetch_group_info_error():
     assert 'error' in response_data
 
 
-def test_fetch_members_count_info_error_group_ids_not_list():
+def test_fetch_groups_info_error_group_ids_not_list():
     with pytest.raises(TypeError):
-        response = fetch_members_count_info('foo')
+        response = fetch_groups_info('foo')
 
 
-def test_fetch_members_count_info_error_group_ids_len_gt_500():
+def test_fetch_groups_info_error_group_ids_len_gt_500():
     with pytest.raises(TypeError):
         group_ids = list(range(501))
-        response = fetch_members_count_info(group_ids)
+        response = fetch_groups_info(group_ids)
 
 
-def test_fetch_members_count_info_empty_response_on_empty_group_ids():
-    response = fetch_members_count_info([])
+def test_fetch_groups_info_empty_response_on_empty_group_ids():
+    response = fetch_groups_info([])
     assert len(response) == 0
 
 
-def test_fetch_members_count_info_empty_success():
-    response = fetch_members_count_info([1])
+def test_fetch_groups_info_empty_success():
+    response = fetch_groups_info([1])
     assert len(response) == 1
-    assert response[0][1] > 0
